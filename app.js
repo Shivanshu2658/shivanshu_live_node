fc29aa27f58c29e40aacc38554cc8c7844812840
Shivanshu2658 <Shivanshuyadav119@gmail.com>
2024-08-07 09:42:47 +0530

    all files

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const env = require("dotenv");
+
+env.config();
+
+const app = express();
+
+app.get("/",(req,res) =>{
+    res.status(200).send("Check this")
+});
+
+app.listen(process.env.PORT || 4500, () =>{
+    console.log("Server Started");
+})
+
+
+
