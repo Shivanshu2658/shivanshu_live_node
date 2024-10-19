74f81fa3817bd9d096085dd3f8037d9165aebdd0
sy143u <sy.uj.008@gmail.com>
2024-10-19 22:52:26 +0530

    new device setup

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const mongoConnet = async ()=> {
     try{
-        await mongoose.connect(process.env.MONGO_URL);
+        await mongoose.connect("mongodb+srv://shivanshuyadav119:shivanshuyadav@dummydb.alhjm24.mongodb.net/");
         console.log("DB connect successful");
     }
     catch(err){
